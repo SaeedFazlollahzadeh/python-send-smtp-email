849c7d491f9b71dabb41bc865f1ed7038c9c1b5b
Saeed <s.fazl73@gmail.com>
2023-04-15 20:44:09 +0330

    adding attachment section

--- mail.py
+++ mail.py
@@ -2,6 +2,8 @@ import smtplib
 import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 sender_email = 'your_email@gmail.com'
 receiver_email = input('Enter email address: ')
@@ -12,11 +14,19 @@ message['Subject'] = 'YOUR SUBJECT'
 message['From'] = sender_email
 message['To'] = receiver_email
 
+# path and file to attach. Don't forget to change MIMEBase according to your file's mime type.
+path = r'/path/to/'
+file_name = r'your_file.pdf'
+attachment = path + file_name
+part = MIMEBase('application', "pdf")  # Don't forget to change MIMEBase according to your file's mime type.
+part.set_payload(open(attachment, "rb").read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment', filename=file_name)
+message.attach(part)
+
 # Create the plain-text and HTML version of your message
 html = '''<p>Your HTML Codes go here</p>
 <p>This is another HTML paragraph.'''
-# Turn these into plain/html MIMEText objects
-# part1 = MIMEText(text, 'plain')
 part1 = MIMEText(html, 'plain')
 part2 = MIMEText(html, 'html')
 
